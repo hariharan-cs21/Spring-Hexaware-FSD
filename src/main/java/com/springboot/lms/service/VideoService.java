986f1536ba0c5f0e255531fae6a446884f79dd65
Hari <hariharant.cs21@bitsathy.ac.in>
2025-06-03 10:52:01 +0530

    Added transaction annotation

--- src/main/java/com/springboot/lms/service/VideoService.java
+++ src/main/java/com/springboot/lms/service/VideoService.java
@@ -3,6 +3,7 @@ package com.springboot.lms.service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.springboot.lms.model.CModule;
 import com.springboot.lms.model.Video;
@@ -18,9 +19,14 @@ public class VideoService {
 		this.videoRepository = videoRepository;
 		this.moduleRepository = moduleRepository;
 	}
-	public List<Video>addAll(int moduleId,List<Video>videos){
-		CModule module = moduleRepository.findById(moduleId).orElseThrow(() -> new RuntimeException("Module not found"));
-		videos.parallelStream().forEach(v->v.setModule(module));
+
+	@Transactional
+	public List<Video> addAll(int moduleId, List<Video> videos) {
+		CModule module = moduleRepository.findById(moduleId)
+				.orElseThrow(() -> new RuntimeException("Module not found"));
+		if (videos.isEmpty())
+			throw new RuntimeException("No Data Found");
+		videos.parallelStream().forEach(v -> v.setModule(module));
 		return videoRepository.saveAll(videos);
 	}
 
